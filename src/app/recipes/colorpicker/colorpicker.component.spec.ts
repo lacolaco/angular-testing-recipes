95b552a73cc6c232440424111023ba36355b624a
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-06-30 23:05:52 +0900

    feat(recipes): add colorpicker recipe (#2)

---
+++ src/app/recipes/colorpicker/colorpicker.component.spec.ts
@@ -0,0 +1,142 @@
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { render } from '@testing-library/angular';
+import { ColorpickerModule } from './colorpicker.module';
+
+describe('ColorpickerComponent', () => {
+  it('should show a listbox for colors', async () => {
+    const { getByRole } = await render(`<app-colorpicker></app-colorpicker>`, {
+      imports: [ColorpickerModule],
+    });
+    expect(getByRole('listbox')).toBeInTheDocument();
+  });
+
+  it('should show the color options', async () => {
+    const colors = ['#fff', '#eee'];
+    const { getAllByRole } = await render(
+      `<app-colorpicker [colors]="colors"></app-colorpicker>`,
+      {
+        imports: [ColorpickerModule],
+        componentProperties: { colors },
+      },
+    );
+    expect(getAllByRole('option').map((el) => el.title)).toEqual(colors);
+  });
+
+  it('should set selected color empty by default', async () => {
+    const { queryAllByRole, getByRole } = await render(
+      `<app-colorpicker [colors]="colors"></app-colorpicker>`,
+      {
+        imports: [ColorpickerModule],
+        componentProperties: { colors: ['#fff', '#eee'] },
+      },
+    );
+    expect(queryAllByRole('option', { selected: true }).length).toBe(0);
+    expect(getByRole('listbox')).not.toHaveAttribute('aria-activedescendant');
+  });
+
+  it('should set selected color on click a color option', async () => {
+    const { getByTitle, queryAllByRole, getByRole, detectChanges } =
+      await render(`<app-colorpicker [colors]="colors"></app-colorpicker>`, {
+        imports: [ColorpickerModule],
+        componentProperties: { colors: ['#fff', '#eee'] },
+      });
+    getByTitle('#fff').click();
+    detectChanges();
+    const selectedOptions = queryAllByRole('option', { selected: true });
+    expect(selectedOptions.length).toBe(1);
+    expect(selectedOptions[0]).toHaveAttribute('title', '#fff');
+    expect(getByRole('listbox')).toHaveAttribute(
+      'aria-activedescendant',
+      selectedOptions[0].id,
+    );
+  });
+
+  it('should set selected color from [value] input', async () => {
+    const { getByRole, rerender } = await render(
+      `<app-colorpicker [colors]="colors" [value]="selectedValue"></app-colorpicker>`,
+      {
+        imports: [ColorpickerModule],
+        componentProperties: {
+          colors: ['#fff', '#eee'],
+          selectedValue: '#eee',
+        },
+      },
+    );
+    expect(getByRole('option', { selected: true })).toHaveAttribute(
+      'title',
+      '#eee',
+    );
+    rerender({ selectedValue: '#fff' });
+    expect(getByRole('option', { selected: true })).toHaveAttribute(
+      'title',
+      '#fff',
+    );
+  });
+
+  it('should emit (valueChange) event on click a color option', async () => {
+    const onChange = jest.fn();
+    const { getByTitle } = await render(
+      `<app-colorpicker [colors]="colors" (valueChange)="onChange($event)"></app-colorpicker>`,
+      {
+        imports: [ColorpickerModule],
+        componentProperties: { colors: ['#fff', '#eee'], onChange },
+      },
+    );
+    getByTitle('#fff').click();
+
+    expect(onChange).toBeCalledWith('#fff');
+  });
+
+  describe('with Angular Forms', () => {
+    it('should be set value from form control', async () => {
+      const formControl = new FormControl('#eee');
+      const { getByRole } = await render(
+        `<app-colorpicker [colors]="colors" [formControl]="formControl"></app-colorpicker>`,
+        {
+          imports: [ColorpickerModule, ReactiveFormsModule],
+          componentProperties: {
+            colors: ['#fff', '#eee'],
+            formControl,
+          },
+        },
+      );
+      expect(getByRole('option', { selected: true })).toHaveAttribute(
+        'title',
+        '#eee',
+      );
+    });
+
+    it('should pass the clicked value to form control', async () => {
+      const formControl = new FormControl('#eee');
+      const { getByTitle } = await render(
+        `<app-colorpicker [colors]="colors" [formControl]="formControl"></app-colorpicker>`,
+        {
+          imports: [ColorpickerModule, ReactiveFormsModule],
+          componentProperties: {
+            colors: ['#fff', '#eee'],
+            formControl,
+          },
+        },
+      );
+      getByTitle('#fff').click();
+      expect(formControl.value).toBe('#fff');
+    });
+
+    it('should pass the input value to form control', async () => {
+      const formControl = new FormControl('#eee');
+      const { rerender } = await render(
+        `<app-colorpicker [colors]="colors" [value]="selectedColor" [formControl]="formControl"></app-colorpicker>`,
+        {
+          imports: [ColorpickerModule, ReactiveFormsModule],
+          componentProperties: {
+            colors: ['#fff', '#eee'],
+            selectedColor: null as string | null,
+            formControl,
+          },
+        },
+      );
+      rerender({ selectedColor: '#fff' });
+      expect(formControl.value).toBe('#fff');
+    });
+  });
+});
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { AlertModule } from './alert/alert.module';
 import { ButtonModule } from './button/button.module';
 import { CardModule } from './card/card.module';
+import { ColorpickerModule } from './colorpicker/colorpicker.module';
 import { ToastModule } from './toast/toast.module';
 
 @NgModule({
   declarations: [],
-  exports: [CardModule, ButtonModule, AlertModule, ToastModule],
+  exports: [
+    CardModule,
+    ButtonModule,
+    AlertModule,
+    ToastModule,
+    ColorpickerModule,
+  ],
 })
 export class RecipesModule {}
