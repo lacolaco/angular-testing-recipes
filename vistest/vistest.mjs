2eb939b9f94b3a6895711e4ba366059aec274a20
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-07-19 17:33:23 +0900

    ci: add vistest check mode

--- vistest/vistest.mjs
+++ vistest/vistest.mjs
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
-import { $, nothrow } from 'zx';
+import { $, nothrow, argv } from 'zx';
 
 const snapshotDir = 'vistest/snapshots';
 const reportPath = `vistest/index.html`;
 const workingDir = 'vistest/.tmp';
 const actualDir = `${workingDir}/actual`;
 const diffDir = `${workingDir}/diff`;
+
+const check = argv.check ?? false;
+
+console.log(`Check mode: ${check}`);
 
 // Clear working directory and result
 await $`rm -rf ${workingDir} ${reportPath}`;
@@ -17,4 +21,10 @@ await $`yarn storybook:build`;
 await $`yarn storycap http://localhost:6007 --serverCmd \"npx http-server --port 6007 storybook-static\" --flat --outDir ${actualDir}`;
 
 // Run visual comparison
-await $`yarn reg-cli ${actualDir} ${snapshotDir} ${diffDir} -R ${reportPath} -J ${workingDir}/reg.json`;
+if (check) {
+  await $`yarn reg-cli ${actualDir} ${snapshotDir} ${diffDir} -R ${reportPath} -J ${workingDir}/reg.json`;
+} else {
+  await nothrow(
+    $`yarn reg-cli ${actualDir} ${snapshotDir} ${diffDir} -R ${reportPath} -J ${workingDir}/reg.json`,
+  );
+}
