36254164a3c4a122e34a1bf880141abb00a44f05
Suguru Inatomi <suguru.inatomi@gmail.com>
2023-07-02 14:14:41 +0900

    fix: upgrade angular to 16

--- src/app/recipes/api-service/api.service.ts
+++ src/app/recipes/api-service/api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Recipe } from './model';
 
@@ -10,9 +11,6 @@ export class ApiService {
   constructor(private readonly http: HttpClient) {}
 
   async getRecipes(): Promise<Recipe[]> {
-    return this.http
-      .get<{ data: Recipe[] }>('/api/recipes')
-      .pipe(map((resp) => resp.data))
-      .toPromise();
+    return lastValueFrom(this.http.get<{ data: Recipe[] }>('/api/recipes').pipe(map((resp) => resp.data)));
   }
 }
